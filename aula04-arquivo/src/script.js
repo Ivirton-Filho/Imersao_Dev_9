337750cdd658c71f5aeac04b7abedcebfc7b39f9
Ivirton-Filho <ivirtonfilho@gmail.com>
2025-04-07 12:02:37 -0300

    fourth commit

--- aula04-arquivo/src/script.js
+++ aula04-arquivo/src/script.js
@@ -8,10 +8,11 @@ let forcaViloes = 0
     let escolhaPersonagem = prompt("Escolha o nome do personagem")
      personagem[i] = escolhaPersonagem
 }
+let viloesPosiveis = [ "Scar", "Malévola", "Úrsula", "Jafar", "Hades", "Gaston", "Capitão Gancho", "Rainha Má", "Cruella de Vil", "Dr. Facilier"]
+
     for (let i = 0; i<3 ; i++) {
-      let indiceAleatorio = Math.floor(Math.random() * 3) + 1
-       let viloesPosiveis = [ "Scar", "Malévola", "Úrsula", "Jafar", "Hades", "Gaston", "Capitão Gancho", "Rainha Má", "Cruella de Vil", "Dr. Facilier"]
-        viloes[i] = viloesPossiveis[indiceAleatorio]
+      let indiceAleatorio = Math.floor(Math.random() * viloesPosiveis.length)
+        viloes[i] = viloesPosiveis[indiceAleatorio]
         alert(viloes)
     }    
 }
