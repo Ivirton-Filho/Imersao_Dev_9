5ecb721119cab233a246bb1d107ed71c04023669
Ivirton-Filho <ivirtonfilho@gmail.com>
2025-04-07 11:09:50 -0300

    third commit

--- aula04-arquivo/src/script.js
+++ aula04-arquivo/src/script.js
@@ -1,14 +1,18 @@
+document.getElementById("resultadoBtn").style.display = "block";
 function jogar () {
 let personagem = ["", "", ""]
 let viloes = ["", "", ""]
 let forcaPersonagem = 0
 let forcaViloes = 0
     for (let i = 0; i<3; i++){
-    escolhaPersonagem = prompt("Escolha o nome do personagem")
-    personagem[i] = escolhaPersonagem
+    let escolhaPersonagem = prompt("Escolha o nome do personagem")
+     personagem[i] = escolhaPersonagem
 }
     for (let i = 0; i<3 ; i++) {
-        
+      let indiceAleatorio = Math.floor(Math.random() * 3) + 1
+       let viloesPosiveis = [ "Scar", "Malévola", "Úrsula", "Jafar", "Hades", "Gaston", "Capitão Gancho", "Rainha Má", "Cruella de Vil", "Dr. Facilier"]
+        viloes[i] = viloesPossiveis[indiceAleatorio]
+        alert(viloes)
     }    
 }
 
