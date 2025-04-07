08957a226492471b5c62c0e36c9688622c9c9b2a
Ivirton-Filho <ivirtonfilho@gmail.com>
2025-04-07 13:50:32 -0300

    fifth commit

--- aula04-arquivo/src/script.js
+++ aula04-arquivo/src/script.js
@@ -2,18 +2,29 @@ document.getElementById("resultadoBtn").style.display = "block";
 function jogar () {
 let personagem = ["", "", ""]
 let viloes = ["", "", ""]
-let forcaPersonagem = 0
+let forcaDoPersonagem = 0
 let forcaViloes = 0
     for (let i = 0; i<3; i++){
-    let escolhaPersonagem = prompt("Escolha o nome do personagem")
+     escolhaPersonagem = prompt("Escolha o nome do personagem")
      personagem[i] = escolhaPersonagem
+     forcaDoPersonagem = forcaDoPersonagem + Math.floor(Math.random() * 10) + 1
 }
+
 let viloesPosiveis = [ "Scar", "Malévola", "Úrsula", "Jafar", "Hades", "Gaston", "Capitão Gancho", "Rainha Má", "Cruella de Vil", "Dr. Facilier"]
 
     for (let i = 0; i<3 ; i++) {
       let indiceAleatorio = Math.floor(Math.random() * viloesPosiveis.length)
         viloes[i] = viloesPosiveis[indiceAleatorio]
-        alert(viloes)
+         forcaViloes = forcaViloes + Math.floor(Math.random() * 10) + 1
+         //cuidado a colocar "let" dentro de loops, pois cria uma variavel do mesmo nome que está fora do loop
     }    
+    alert(viloes)
+    if(forcaDoPersonagem > forcaViloes){
+        alert(`Os personagens ${personagem} VENCERAM o cabo de guerra.A força foi dos personagens foi ${forcaDoPersonagem} e a dos vilões ${forcaViloes}`)
+    }else if (forcaDoPersonagem < forcaViloes){
+        alert(`Os vilões ${viloes} VENCERAM o cabo de guerra.A força foi dos vilões foi${forcaViloes} e a dos personagens foi de ${forcaDoPersonagem}`)
+    }else{
+        alert("EMPATE!.O cabo de guerra partiu no meio")
+    }
 }
 
