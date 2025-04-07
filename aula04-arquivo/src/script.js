88a5484d9b59f7f7561d317ab99ac037ba408cf8
Ivirton-Filho <ivirtonfilho@gmail.com>
2025-04-07 09:19:33 -0300

    second commit

---
+++ aula04-arquivo/src/script.js
@@ -0,0 +1,14 @@
+function jogar () {
+let personagem = ["", "", ""]
+let viloes = ["", "", ""]
+let forcaPersonagem = 0
+let forcaViloes = 0
+    for (let i = 0; i<3; i++){
+    escolhaPersonagem = prompt("Escolha o nome do personagem")
+    personagem[i] = escolhaPersonagem
+}
+    for (let i = 0; i<3 ; i++) {
+        
+    }    
+}
+
